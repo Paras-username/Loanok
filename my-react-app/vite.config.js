3005d1a9b433494d8f8a9918087287aa5e47566d
Paras-username <parasporia4@gmail.com>
2025-03-20 19:37:47 +0530

    Your commit message

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -1,8 +1,34 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       '@': '/src',
+//     },
+//   },
+//   server: {
+//     host: true,
+//     strictPort: true,
+//     cors: true,  // Enable CORS
+//     origin: "https://d054-2409-40d6-1010-db96-a135-7f67-701f-3ba.ngrok-free.app", // Allow Ngrok URL
+//     allowedHosts: ["d479-59-96-46-44.ngrok-free.app"], // Add your blocked Ngrok host here
+//   },
+// });
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import sitemap from 'vite-plugin-sitemap';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://www.loanok.in', // Your website URL
+      dynamicRoutes: [ '/Loan', '/calculator' , '/aboutus' , '/privacy' , '/terms'], // Add important dynamic routes
+    }),
+  ],
   resolve: {
     alias: {
       '@': '/src',
